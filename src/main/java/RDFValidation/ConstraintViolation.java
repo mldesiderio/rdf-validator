5dfca306140dfe39bad88a7aed7c14727a575b8f
Thomas Bosch <thomas.bosch@gesis.org>
2014-07-23 14:44:07 +0200

    reorganized structure

---
+++ src/main/java/RDFValidation/ConstraintViolation.java
@@ -0,0 +1,43 @@
+package RDFValidation;
+
+import java.util.List;
+
+public class ConstraintViolation 
+{
+	String source = null;
+	String root = null;
+	String message = null;
+	List<String> paths = null;
+	List<String> fixes = null;
+	
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getRoot() {
+		return root;
+	}
+	public void setRoot(String root) {
+		this.root = root;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public List<String> getPaths() {
+		return paths;
+	}
+	public void setPaths(List<String> paths) {
+		this.paths = paths;
+	}
+	public List<String> getFixes() {
+		return fixes;
+	}
+	public void setFixes(List<String> fixes) {
+		this.fixes = fixes;
+	}
+}
